cdcab0825f3cc25446aac6fb4aa71d48f9059e86
jasenzephaniah <jasenzephaniah11@gmail.com>
2026-04-24 19:14:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -165,4 +165,5 @@ app.post("/bfhl", (req, res) => {
         }
     });
 });
-app.listen(3000, () => console.log("NEW SERVER RUNNING"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running"));
